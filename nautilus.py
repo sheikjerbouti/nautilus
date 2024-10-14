3421b33d5fdee5cafa5cc7b9813e2fce28d4a928
Nico Liberato <nicoliberatoc@gmail.com>
2024-10-14 03:16:25 +0200

    Add the cluster info command

--- nautilus.py
+++ nautilus.py
@@ -31,6 +31,27 @@ class DescribeK8s:
         self.config = config.load_kube_config()    
         self.namespace = namespace
         self.v1 = client.CoreV1Api()
+        
+    def get_cluster_info(self):
+        """
+        Retrieve and print information about the Kubernetes cluster.
+
+        This method fetches the API resources available in the cluster and prints
+        the number of resources. It also prints the URLs for the KubeDNS service
+        and the Kubernetes control plane.
+
+        Returns:
+            None
+        """
+        
+        api_resources = self.get_api_resources()
+        if api_resources:
+            print(f"API Resources: {len(api_resources.resources)}")
+
+        host = self.v1.api_client.configuration.host
+        print(
+            f"KubeDNS is running at http://{host}/api/v1/namespaces/kube-system/services/kube-dns:dns/proxy")
+        print(f"Kubernetes control plane is running at {host}")
 
 
 def main():
@@ -42,8 +63,8 @@ def main():
     try:
         k8s_cluster = DescribeK8s("default")
     except ConfigException:
-        print("Invalid kube-config file. No configuration found.")
-    
+        cprint("Invalid kube-config file. No configuration found.", "red")
+        return
 
 
 if __name__ == "__main__":
